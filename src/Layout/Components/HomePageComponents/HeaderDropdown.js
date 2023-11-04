1b066a571c5ecf915f3f9e7ae9f35672ab2cfe10
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2023-11-04 14:58:14 +0300

    dropdorwn added

--- src/Layout/Components/HomePageComponents/HeaderDropdown.js
+++ src/Layout/Components/HomePageComponents/HeaderDropdown.js
@@ -3,39 +3,45 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Menu, Transition } from "@headlessui/react";
 import React, { Fragment } from "react";
 import { useSelector } from "react-redux";
-import DropdownButton from "../../Compounds/HomePageCompounds/DropdownButton";
+import { Link } from "react-router-dom";
 
 const HeaderDropdown = () => {
   const productCategories = useSelector((state) => state.global.categories);
   return (
-    <Menu as="nav">
-      {({ open }) => (
-        <>
-          <Menu.Button>
-            <FontAwesomeIcon icon={faChevronDown} />
-          </Menu.Button>
-          {open && (
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items>
-                {({ active }) =>
-                  active &&
-                  productCategories.map((category, index) => (
-                    <DropdownButton key={index} category={category} />
-                  ))
-                }
-              </Menu.Items>
-            </Transition>
-          )}
-        </>
-      )}
+    <Menu as="div" className="relative inline-block text-left z-50">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-center items-center  px-2 text-sm font-medium text-black">
+          <FontAwesomeIcon icon={faChevronDown} />
+        </Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+          <div className="px-1 py-1 ">
+            {productCategories.map((product) => (
+              <Menu.Item key={product.id}>
+                {({ active }) => (
+                  <Link
+                    to={`/shopping/${product.gender}/${product.code}`}
+                    className={`${
+                      active ? "bg-violet-500 text-black" : "text-gray-400"
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    {product.title}
+                  </Link>
+                )}
+              </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
+      </Transition>
     </Menu>
   );
 };
